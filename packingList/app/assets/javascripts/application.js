fd9176ac61a02c765278f538d9688bc783b344ad
Anya Melnyk <gannasachakov@gmail.com>
2015-06-03 23:08:24 -0400

    added location autocomplete to the destination fields

--- packingList/app/assets/javascripts/application.js
+++ packingList/app/assets/javascripts/application.js
@@ -15,3 +15,37 @@
 //= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+  $(function() {
+    function log( message ) {
+      $( "<div>" ).text( message ).prependTo( "#log" );
+      $( "#log" ).scrollTop( 0 );
+    }
+ 
+    $( ".location" ).autocomplete({
+      source: function( request, response ) {
+        $.ajax({
+          url: "http://gd.geobytes.com/AutoCompleteCity",
+          dataType: "jsonp",
+          data: {
+            q: request.term
+          },
+          success: function( data ) {
+            response( data );
+          }
+        });
+      },
+      minLength: 3,
+      select: function( event, ui ) {
+        log( ui.item ?
+          "Selected: " + ui.item.label :
+          "Nothing selected, input was " + this.value);
+      },
+      open: function() {
+        $( this ).removeClass( "ui-corner-all" ).addClass( "ui-corner-top" );
+      },
+      close: function() {
+        $( this ).removeClass( "ui-corner-top" ).addClass( "ui-corner-all" );
+      }
+    });
+  });
