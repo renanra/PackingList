fece0fa2c8d4b072f0bdcd75d5f68d8746ee3655
Renan de Azevedo <renan.razevedo@gmail.com>
2015-09-24 22:59:53 -0300

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  get '/calendar' => 'calendar#index'
-  get '/test_page' => 'test_page#index'
-  get '/trips/:id/activity_items_trip', to: 'trips#activity_items_trip', as: :activity_items_trip
-  get '/trips/:id/saved_items_trip' => "trips#saved_items_trip", as: :saved_items_trip
-
-  post '/trips/:id', to: 'trips#create_default_items_trip', as: :create_default_items_trip
-
-  delete '/trips', to: 'trips#destroy', as: :delete
+  # get '/test_page' => 'test_page#index'
+  # post '/trips/:id', to: 'trips#create_default_items_trip', as: :create_default_items_trip
+  # get '/trips/:id/activity_items_trip', to: 'trips#activity_items_trip', as: :activity_items_trip
 
   resources :trips do
-    put :update_default_items, on: :member
+    member do
+      get :choose_activities
+
+      put :update_activities
+      put :update_default_items
+    end
+
   end
 
   resources :items
-  resources :calendar
+  resource :calendar, only: [:show]
 
   namespace :api do
     resources :default_items
